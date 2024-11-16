774e439f67cfe76885248fad3e0c224b261c3e40
Alpha Roger (anshumansinha3301) <161927447+rogercoding@users.noreply.github.com>
2024-11-16 13:48:29 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -9,7 +9,7 @@ public class QuickSort {
     }
 
     private static int partition(int[] array, int low, int high) {
-        int pivot = array[high];
+        int pivot = array[high];  
         int i = (low - 1); 
 
         for (int j = low; j < high; j++) {
